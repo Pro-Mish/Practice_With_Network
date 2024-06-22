a7298a7496091a6028bf79432df3046c5f2e6590
Pro-Mish <mishackmadubandlela@gmail.com>
2024-06-22 22:37:38 +0200

    baby.py
    This program uses a user's input to verify if they are old enough to enter a party. The minimum age needed is 18 years old. It runs a check based on birth year only though.

--- baby.py
+++ baby.py
@@ -7,11 +7,3 @@ if age >= 18:
     print("Congrats! You are old enough. YOu may enter the party!")
 else:
      print("Unfortunately, you are too young")
-
-print()
-
-
-# def age_calculator(current_year,birth_year):
-#    age = current_year - birth_year
-#    print("The age of the person in years is",age,"years")
-# age_calculator(2023,1995)
